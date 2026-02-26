74fc95833f3c8e9c11fe142017cd01e7cbd5725b
EmanuelOrozco <emanuelorozco2011@hotmail.com>
2026-02-26 14:19:43 -0500

    chore: delete extra files and add .gitignore

--- dist/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let cuenta = 0;
-const boton = document.getElementById('btn');
-const display = document.getElementById('contador');
-boton.addEventListener('click', () => {
-    cuenta++;
-    display.innerText = cuenta.toString();
-});
-//# sourceMappingURL=app.js.map
